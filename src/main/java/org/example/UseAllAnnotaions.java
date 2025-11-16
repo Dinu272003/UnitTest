58276b0f63e1cc339363233ce44a443e6f7ac6c8
Dinesh Datta Shrirame <dineshshrirame27@gmail.com>
2025-11-16 13:25:49 +0530

    first commit

---
+++ src/main/java/org/example/UseAllAnnotaions.java
@@ -0,0 +1,12 @@
+//package org.example;
+//
+//import org.junit.jupiter.api.Test;
+//
+//public class UseAllAnnotaions {
+//
+//    @Test
+//    public  int (int a,int b){
+//        return a+b;
+//    }
+//
+//}
--- src/test/java/org/example/services/TestCalculator.java
+++ src/test/java/org/example/services/TestCalculator.java
@@ -5,62 +5,67 @@ import org.junit.*;
 import org.w3c.dom.ls.LSOutput;
 
 public class TestCalculator {
-   static  {
-       System.out.println("Static block");
-   }
 
-    @Test
+
+    @Test(timeout = 2000)
     public void addTest(){
-        System.out.println("addTest Method...");
-
+//        System.out.println("addTest Method...");
+//        try{
+//            Thread.sleep(3000);
+//
+//        }catch (Exception e){
+//
+//        }
        int res= Calculator.add(12,12);
 
        //actual result
 
         //expected result
         int expected=24;
-        int result =24;
-        Assert.assertEquals(expected,result);
+
+        Assert.assertEquals(expected,res);
 
     }
 
     @Test
     public void subTest(){
-        System.out.println("subTest Method...");
+//        System.out.println("subTest Method...");
         int res=Calculator.sub(12,10);
         int expected=2;
-        int result=2;
-        Assert.assertEquals(expected,result);
+
+        Assert.assertEquals(expected,res);
     }
 
     @Test
 //    @BeforeClass
     public  void  nameTest(){
-        System.out.println("nameTest Method...");
-        String res=Calculator.PrintName("Dinu");
-        String expected="Dinesh";
-        String result="Dinesh";
-
-        Assert.assertEquals(expected,result);
+//        System.out.println("nameTest Method...");
+        String res=Calculator.PrintName("Dinesh");
+        String expected="Dinu";
+//        String result ="Dinesh";
+        Assert.assertEquals(expected,res);
     }
     @BeforeClass
     public static void display(){
-        System.out.println("Before All test Cases in Display Method...1");
+//        System.out.println("Before All test Cases in Display Method...1");
     }
 
     @BeforeClass
     public static void show(){
+
         System.out.println("Before All Test Cases in Show Method....");
     }
 
     //this is excuted after all Mehods..
     @AfterClass
     public static void afterClass(){
+
         System.out.println("After Class Annotation Method...");
     }
 
     @After
     public  void afterClassAnnoOnly(){
+
         System.out.println("After Annotation Only...");
     }
 
